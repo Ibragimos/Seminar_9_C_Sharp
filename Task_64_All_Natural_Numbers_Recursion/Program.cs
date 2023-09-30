8d1ca4aba90bc549dc68e10563b628cbf00f6610
Marina <korhovama@mail.ru>
2023-09-30 23:15:45 +0300

    Add Task_64

---
+++ Task_64_All_Natural_Numbers_Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿int EnteredNumber()
+{
+    System.Console.WriteLine("Enter a number for recursion: ");
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int Rec(int N)
+{
+    if(N == 0) return 0;
+    else 
+    {
+        System.Console.Write($"{N} ");
+        return Rec(N - 1);
+    }
+}
+
+Rec(EnteredNumber());
