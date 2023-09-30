8ef4ff46e4674685a5e495da3ea1f3fa03ae7e1a
Marina <korhovama@mail.ru>
2023-10-01 00:10:13 +0300

    Add Task_68

---
+++ Task_66_Sum_Natural_Numbers_Recursion/Program.cs
@@ -0,0 +1,24 @@
+﻿int MinNum()
+{
+    System.Console.WriteLine("Введите минимальное значение: ");
+    int min = int.Parse(Console.ReadLine()!);
+    return min;
+}
+
+int MaxNum()
+{
+    System.Console.WriteLine("Введите максимальное значение: ");
+    int max = int.Parse(Console.ReadLine()!);
+    return max;
+}
+
+
+int Sum(int M, int N)
+{
+    
+     if (M == N) return N;
+    else return Sum(M + 1, N) + M;
+}
+
+
+System.Console.WriteLine($"{Sum(MinNum(), MaxNum())}");
---
+++ Task_68_Ackerman_Function_Recursion/Program.cs
@@ -0,0 +1,34 @@
+﻿int M()
+{
+    System.Console.WriteLine("Введите M: ");
+    int M = int.Parse(Console.ReadLine()!);
+    return M;
+}
+
+int N()
+{
+    System.Console.WriteLine("Введите N: ");
+    int N = int.Parse(Console.ReadLine()!);
+    return N;
+}
+
+
+int AckermanFunction(int M, int N)
+{
+   if(M == 0) 
+    {
+        return N + 1;
+    }
+    else if(M > 0 && N == 0) 
+    {
+        return AckermanFunction(M - 1, 1);
+    }
+    else if(M > 0 && N > 0) 
+    {
+        return AckermanFunction(M - 1, AckermanFunction(M, N - 1));
+    }
+    else return -1;
+}
+
+int ackermanFunction = AckermanFunction(M(), N());
+System.Console.WriteLine(ackermanFunction);
